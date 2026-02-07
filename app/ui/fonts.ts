06a3b665913e295892966b2df390c3e3e1a49d4b
Amanda <77864882+amansdali@users.noreply.github.com>
2026-02-07 16:51:32 -0500

    Initial commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,10 @@
+import { Inter, Lusitana, Gamja_Flower} from 'next/font/google';
+export const inter = Inter({ subsets: ['latin'] });
+export const lusitana = Lusitana({
+    weight: ['400', '700'],
+    subsets: ['latin'],
+});
+export const gamjaFlower = Gamja_Flower({
+    subsets: ['latin'],
+    weight: '400',
+});
